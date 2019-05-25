a7775dd561c80d0251d547c83aa68ba372949303
azhek <36861037+azhek@users.noreply.github.com>
2019-05-24 20:20:57 -0400

    JeuVideoComplet

--- js/script.js
+++ js/script.js
@@ -1,10 +1,15 @@
 
-
-
-
 $('#imageMedecine').click(function(){
    window.location = 'page2.html'
 });
+
+if($(window).width() < 1200){
+    $('.paragrapheMedecinePage2').html("La technologie est en train de transformer les soins de santé. Les appareils et les applications de santé mobiles donnent de nouveaux moyens de surveiller l’état des patients et de promouvoir la santé.")
+}
+else
+{
+    $('.paragrapheMedecinePage2').html("La technologie est en train de transformer les soins de santé. Les appareils et les applications de santé mobiles donnent de nouveaux moyens de surveiller l’état des patients et de promouvoir la santé. Les innovations en soins virtuels aident les médecins à atteindre les patients des collectivités éloignées. Les mégadonnées et l’analytique produisent des connaissances approfondies sur la santé des populations, les facteurs de morbidité, l’efficacité clinique et bien plus. En outre, la robotique, l’impression 3D, la nanotechnologie, l’Internet des objets et la réalité virtuelle et la réalité augmentée introduiront d’autres possibilités.")
+};
 
 if ($(window).width() > 1199) {
     $('#imageMedecine').hover(function(){
@@ -32,8 +37,51 @@ $('#imageVideo').click(function(){
 
 $('#facebookMedical').click(function(){
     window.open('https://www.facebook.com/BrainMachineInterfaceNews/')
-})
+});
 
 $('#twitterMedical').click(function(){
     window.open('https://twitter.com/MedtronicFR/status/1005115345739210752')
-})
+});
+
+var nbAppuie = 0;
+
+
+$('#boutonRobot').click(function(){
+    if(nbAppuie%2 === 0)
+    {
+        $('.backgroundRobotique2').css('background-image','url(img/loiRobotique.jpg)')
+    }
+
+    else
+    {
+        $('.backgroundRobotique2').css('background-image','url(img/loiRobotique2.jpg)')
+    }
+    nbAppuie++;
+});
+
+
+
+$('#boutonUbisoft').click(function(){
+   $('.imageCompagnieChange').attr("src","img/assassinCreedImage.jpg")
+    $('#titreCompagnie').html('ASSASSIN CREED')
+    $('#paragrapheCompagnie').html('Assassin\'s Creed (littéralement « le Credo de l\'Assassin ») est une série de jeux vidéo historique d\'action-aventure et d\'infiltration en monde ouvert, développée et éditée par Ubisoft. Les titres principaux développés par Ubisoft Montréal sortent sur les consoles de 7e (Xbox 360 et PlayStation 3) et 8e génération (Xbox One, PlayStation 4 et Wii U) ainsi que sur PC, tandis que les épisodes secondaires sortent sur consoles portables (Nintendo DS, PlayStation Portable, PlayStation Vita) et sur téléphones portables (Java, Android, iOS).')
+});
+
+$('#boutonCapcom').click(function(){
+    $('.imageCompagnieChange').attr("src","img/streetFighterImage.jpg")
+    $('#titreCompagnie').html('STREET FIGHTER')
+    $('#paragrapheCompagnie').html('Street Fighter II: The World Warrior (ストリートファイターⅡ, Sutorîto Faitâ Tsū?) est un jeu vidéo de combat développé et édité par Capcom, sorti en 1991 sur système d\'arcade CP System, et sujet à diverses adaptations, dont la plus célèbre, sur Super Nintendo. Le jeu fait s\'affronter plusieurs personnages dans des combats en un-contre-un. Le premier joueur mettant KO son adversaire par deux fois, sort victorieux du combat')
+});
+
+$('#boutonBethesda').click(function(){
+    $('.imageCompagnieChange').attr("src","img/skyrimImage.jpg")
+    $('#titreCompagnie').html('SKYRIM')
+    $('#paragrapheCompagnie').html('The Elder Scrolls V: Skyrim (souvent abrégé en Skyrim) est un jeu vidéo de rôle et d\'action développé par Bethesda Game Studios et édité par Bethesda Softworks, sorti le 11 novembre 2011 sur PlayStation 3, Xbox 360 et Microsoft Windows. C\'est le cinquième opus de la série de jeux The Elder Scrolls, après Arena, Daggerfall, Morrowind et Oblivion.')
+});
+
+$('#boutonBlizzard').click(function(){
+    $('.imageCompagnieChange').attr("src","img/worldofwarcraftImage.jpg")
+    $('#titreCompagnie').html('WORLD OF WARCRAFT')
+    $('#paragrapheCompagnie').html('World of Warcraft (abrégé WoW) est un jeu vidéo de type MMORPG (jeu de rôle en ligne massivement multijoueur) développé par la société Blizzard Entertainment. C\'est le 4e jeu de l\'univers médiéval-fantastique Warcraft, introduit par Warcraft: Orcs and Humans en 1994. World of Warcraft prend place en Azeroth, près de quatre ans après les événements de la fin du jeu précédent, Warcraft III: The Frozen Throne1 Blizzard Entertainment annonce World of Warcraft le 2 septembre 20012. Le jeu est sorti en Amérique du Nord le 23 novembre 2004, pour les 10 ans de la franchise Warcraft.')
+});
+
